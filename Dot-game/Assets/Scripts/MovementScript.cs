064f07b665275cc35903aa8c76a3890172eeabb7
Don6l <donalmcclean@gmail.com>
2021-09-16 16:54:49 +0100

    Basic dot movement and colour

---
+++ Dot-game/Assets/Scripts/MovementScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 5f;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        calculateMovement();
+    }
+
+
+    void calculateMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 newDirection = new Vector3(horizontalInput, verticalInput, 0f);
+        transform.Translate(newDirection * _speed * Time.deltaTime);
+    }
+    
+}
